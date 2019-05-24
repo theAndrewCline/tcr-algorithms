7c8d28ddb655f02d3377bbe936dd8b9750dd86e8
Andrew Cline <andrew.cline77@gmail.com>
2019-05-24 10:47:42 -0500

    Add Settings to run TCR on Save (#2)
    * add first test and setup
    
    * TCR @ Fri May 24 07:57:45 2019
    
    * TCR @ Fri May 24 08:11:36 2019
    
    * TCR @ Fri May 24 09:56:46 2019
    
    * TCR @ Fri May 24 09:58:23 2019
    
    * TCR @ Fri May 24 09:59:14 2019
    
    * TCR @ Fri May 24 10:05:36 2019
    
    * TCR @ Fri May 24 10:17:37 2019
    
    * TCR @ Fri May 24 10:38:35 2019
    
    * TCR @ Fri May 24 10:38:42 2019
    
    * TCR @ Fri May 24 10:39:29 2019

--- example.test.js
+++ example.test.js
@@ -2,6 +2,30 @@ function add (a, b) {
   return a + b
 }
 
+function subtract (a, b) {
+  return a - b
+}
+
+function multiply (a, b) {
+  return a * b
+}
+
+function divide (a, b) {
+  return a / b
+}
+
 test('add', () => {
   expect(add(1, 2)).toBe(3)
+})
+
+test('subtract', () => {
+  expect(subtract(1, 2)).toBe(-1)
+})
+
+test('multiply', () => {
+  expect(multiply(1, 2)).toBe(2)
+})
+
+test('divide', () => {
+  expect(divide(4, 2)).toBe(2)
 })
