cc4c53bef6c551d0795b40c0b57ef6b8d741ef39
Andrew Cline <andrew.cline77@gmail.com>
2019-05-24 08:11:36 -0500

    TCR @ Fri May 24 08:11:36 2019

--- firstTest.js
+++ firstTest.js
@@ -2,6 +2,12 @@ function firstTest () {
   return true
 }
 
+function add (a, b) {
+  return a + b
+}
+
+
 module.exports = {
-  firstTest
+  firstTest,
+  add
 }
--- firstTest.test.js
+++ firstTest.test.js
@@ -1,7 +1,13 @@
-const { firstTest } = require('./firstTest')
+const { firstTest, add } = require('./firstTest')
 
 describe('firstTest', () => {
   it('should return true', () => {
     expect(firstTest()).toBe(true)
+  })
+})
+
+describe('add', () => {
+  it('should add two numbers', () => {
+    expect(add(1, 2)).toBe(3)
   })
 })
