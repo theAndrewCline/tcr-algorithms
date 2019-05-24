90cb60729f3b91a303ae6ea576df7fe292d5c454
Andrew Cline <andrew.cline77@gmail.com>
2019-05-24 10:17:37 -0500

    TCR @ Fri May 24 10:17:37 2019

--- example.test.js
+++ example.test.js
@@ -2,6 +2,22 @@ function add (a, b) {
   return a + b
 }
 
+function subtract (a, b) {
+  return a - b
+}
+
+function multiply (a, b) {
+  return a * b
+}
+
 test('add', () => {
   expect(add(1, 2)).toBe(3)
+})
+
+test('subtract', () => {
+  expect(subtract(1, 2)).toBe(-1)
+})
+
+test('multiply', () => {
+  expect(multiply(1, 2)).toBe(2)
 })
